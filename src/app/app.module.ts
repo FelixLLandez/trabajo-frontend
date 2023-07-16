d28862b3abccb201f86dccba01243641600ae1a7
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-07-16 15:48:36 -0600

    Creacion de la carpeta Sidebar con componentes para el SIDEBAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './Sidebar/body/body.component';
+import { SidenavComponent } from './Sidebar/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
