13d1cff406ff24a95d5db59353f859dab6ee3c76
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-07-16 15:18:13 -0600

    Diseño de la interfaz de LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login-administrador', component:LoginAdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
