9caaa02926b33094a0ed318b8e82c9c8a135e620
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-07-16 17:16:57 -0600

    Diseño del componente de Administradores en el RolAdministrador

---
+++ src/app/Sidebar/sidenav/nav-data.ts
@@ -0,0 +1,25 @@
+export const navbarData=[
+    {
+        routeLink:'ver-administradores',
+        icon: 'fal fa-users-gear',
+        label:'Administradores',
+        title:'Administradores'
+        
+    },
+    {
+        routeLink:'solicitantes',
+        icon: 'fal fa-newspaper',
+        label:'Solicitantes',
+        title:'Solicitantes'
+        
+    },
+    {
+        routeLink:'postulantes',
+        icon: 'fal fa-magnifying-glass-dollar',
+        label:'Postulanes',
+        title:'Postulantes'
+   
+    },
+    
+
+]
---
+++ src/app/pages/administradores-rol-admin/administradores-rol-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administradores-rol-admin',
+  templateUrl: './administradores-rol-admin.component.html',
+  styleUrls: ['./administradores-rol-admin.component.css']
+})
+export class AdministradoresRolAdminComponent {
+
+}
