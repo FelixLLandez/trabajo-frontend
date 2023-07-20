adc2c7ad572242ae338c02e7e8375cb2d5ce4d8f
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-07-20 14:16:59 -0600

    Cambios generales a los diseños de las cards de los trabajos publicados por el solicitante en el RolAdministrador

--- src/app/pages/Rol_Administrador/Solicitantes/ver-solicitante-rol-admin/ver-solicitante-rol-admin.component.ts
+++ src/app/pages/Rol_Administrador/Solicitantes/ver-solicitante-rol-admin/ver-solicitante-rol-admin.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./ver-solicitante-rol-admin.component.css']
 })
 export class VerSolicitanteRolAdminComponent {
+  items = Array(10).fill(0);
 
+  constructor() {
+   // main.ts
+// main.ts
+document.addEventListener("DOMContentLoaded", function () {
+  const btnUp = document.getElementById("btnUp");
+
+  if (btnUp) {
+    btnUp.addEventListener("click", scrollToTop);
+    window.addEventListener("scroll", toggleButtonVisibility);
+  }
+
+  function scrollToTop() {
+    if ("scrollTo" in window) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      document.documentElement.scrollTop = 0;
+    }
+  }
+
+  function toggleButtonVisibility() {
+    const offset = 100; // Mostrar el botón cuando falten 100px para llegar al final de la página
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+
+    if (btnUp) {
+      if (scrollPosition > pageHeight - offset) {
+        btnUp.classList.add("show");
+      } else {
+        btnUp.classList.remove("show");
+      }
+    }
+  }
+});
+
+
+
+  }
 }
