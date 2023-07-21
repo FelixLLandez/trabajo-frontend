5b5bf6c0d04e288b5adf3caea1242a3292b621fc
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-07-21 12:26:00 -0600

    Diseño del SweetAlert para inhabilitar a un Postulante en el RolAdministrador

--- src/app/pages/Rol_Administrador/Postulantes/postulantes-rol-admin/postulantes-rol-admin.component.ts
+++ src/app/pages/Rol_Administrador/Postulantes/postulantes-rol-admin/postulantes-rol-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-postulantes-rol-admin',
@@ -39,6 +40,22 @@ export class PostulantesRolAdminComponent {
   }
 
   eliminar_postulante() {
-
+    Swal.fire({
+      title: 'Estás seguro de desactivar este postulante?',
+      text: "Podrás volver a habilitarlo si es necesario",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, desactivar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Desactivado exitosamente!',
+        })
+      }
+    })
   }
 }
