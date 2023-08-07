f1888a0e6a8e8396266503a013a1bb9fee38eab2
isidronoe1234 <133019163+isidronoe1234@users.noreply.github.com>
2023-08-07 09:19:53 -0600

    Implementación de boton de cerrar sesión

--- src/app/pages/Rol_Administrador/Solicitantes/solicitantes-rol-admin/solicitantes-rol-admin.component.ts
+++ src/app/pages/Rol_Administrador/Solicitantes/solicitantes-rol-admin/solicitantes-rol-admin.component.ts
@@ -87,4 +87,31 @@ export class SolicitantesRolAdminComponent implements OnInit, OnDestroy {
       }, 1);
     })
   }
+
+  salir() {
+    Swal.fire({
+      title: '¿Estás seguro de cerrar sesón?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        Swal.fire({
+          icon: 'success',
+          title: 'Cerrando sesión...',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        setTimeout(() => {
+          this.router.navigateByUrl(`/login-administrador`);
+        }, 1500);
+      }
+    })
+  }
 }
